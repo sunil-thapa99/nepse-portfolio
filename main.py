b61c420c01a53f36229705a1712afd796f94eb7c
Sunil Thapa <lappanchappan43@gmail.com>
2026-04-03 20:44:32 -0500

    Adding chromedriver for github action

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ def build_driver(
         options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
